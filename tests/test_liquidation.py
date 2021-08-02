ab1ce7244d1ad2474aede4a1ce268bd1282abb2f
Entreprenerd <valeranijr@gmail.com>
2021-08-02 21:40:34 +0200

    fix: tests functions

--- tests/test_liquidation.py
+++ tests/test_liquidation.py
@@ -86,13 +86,8 @@ def test_liquidation(
     chain.mine(1)
 
     ## Avoid random reverts - Empty rewards and swap manually
-    levered_strat.setMin(
-        levered_strat.minHealth(),
-        0,
-        0,
-        levered_strat.minRebalanceAmount(),
-        {"from": gov},
-    )
+    levered_strat.setMinStkAAVEPRice(0, {"from": gov})
+    levered_strat.setMinAAVEWantToPrice(0, {"from": gov})
     chain.mine(1)
 
     ## Call harvest
