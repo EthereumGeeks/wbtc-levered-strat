c3feef72b7c342f690c64cb501026adc489f7c0d
Entreprenerd <valeranijr@gmail.com>
2021-08-16 16:49:46 +0200

    fix: formatting

--- scripts/stats.py
+++ scripts/stats.py
@@ -3,76 +3,103 @@ from brownie import *
 
 AT = "0xDD387F2fe0D9B1E5768fc941e7E48AA8BfAf5e41"
 
+
 def main():
 
-  strat = Strategy.at(AT)
-  vault = interface.VaultAPI(strat.vault())
+    strat = Strategy.at(AT)
+    vault = interface.VaultAPI(strat.vault())
 
-  [performanceFee, activation, debtRatio, minDebtPerHarvest, maxDebtPerHarvest, lastReport, totalDebt, totalGain, totalLoss] = vault.strategies(strat)
+    [
+        performanceFee,
+        activation,
+        debtRatio,
+        minDebtPerHarvest,
+        maxDebtPerHarvest,
+        lastReport,
+        totalDebt,
+        totalGain,
+        totalLoss,
+    ] = vault.strategies(strat)
 
-  
-  print("Reporting for wBTC Strat")
-  print(strat.name())
+    print("Reporting for wBTC Strat")
+    print(strat.name())
 
-  print("Address")
-  print(AT)
+    print("Address")
+    print(AT)
 
-  print("Total Assets Estimated")
-  print(strat.estimatedTotalAssets())
+    print("Total Assets Estimated")
+    print(strat.estimatedTotalAssets())
 
-  print("Of which rewards")
-  print(strat.valueOfRewards())
+    print("Of which rewards")
+    print(strat.valueOfRewards())
 
-  print("Total deposited")
-  print(strat.deposited())
+    print("Total deposited")
+    print(strat.deposited())
 
-  print("Total borrowed")
-  print(strat.borrowed())
+    print("Total borrowed")
+    print(strat.borrowed())
 
-  print("Total Underlying tokens, i.e. totalDebt")
-  print(totalDebt)
-  
+    print("Total Underlying tokens, i.e. totalDebt")
+    print(totalDebt)
 
-  leverage_ratio = strat.deposited() / totalDebt
+    leverage_ratio = strat.deposited() / totalDebt
 
-  print("Leverage Ratio")
-  print(leverage_ratio)
+    print("Leverage Ratio")
+    print(leverage_ratio)
 
-  print("LTV")
-  print(strat.borrowed() / strat.deposited())
+    print("LTV")
+    print(strat.borrowed() / strat.deposited())
 
-  print("Can lever up with another")
-  print(strat.canBorrow())
+    print("Can lever up with another")
+    print(strat.canBorrow())
 
-
-  """
+    """
     TODO
     Expected Profit: 26,620.334323
     Time to liquidation: 101.72 weeks
   """
-  lending_pool = interface.ILendingPool(strat.LENDING_POOL())
+    lending_pool = interface.ILendingPool(strat.LENDING_POOL())
 
-  [configuration, liquidityIndex, variableBorrowIndex, currentLiquidityRate, currentVariableBorrowRate, currentStableBorrowRate, lastUpdateTimestamp, aTokenAddress, stableDebtTokenAddress, variableDebtTokenAddress, interestRateStrategyAddress, id] = lending_pool.getReserveData(strat.want())
+    [
+        configuration,
+        liquidityIndex,
+        variableBorrowIndex,
+        currentLiquidityRate,
+        currentVariableBorrowRate,
+        currentStableBorrowRate,
+        lastUpdateTimestamp,
+        aTokenAddress,
+        stableDebtTokenAddress,
+        variableDebtTokenAddress,
+        interestRateStrategyAddress,
+        id,
+    ] = lending_pool.getReserveData(strat.want())
 
-  print("currentVariableBorrowRate")
-  print(currentVariableBorrowRate)
-  
-  print("currentLiquidityRate")
-  print(currentLiquidityRate)
+    print("currentVariableBorrowRate")
+    print(currentVariableBorrowRate)
 
-  liquidation_amount = strat.deposited() * .75
+    print("currentLiquidityRate")
+    print(currentLiquidityRate)
 
-  debt_to_liquidation = liquidation_amount - strat.borrowed() ## The debt that will liquidate us
+    liquidation_amount = strat.deposited() * 0.75
 
-  print("debt_to_liquidation")
-  print(debt_to_liquidation)
+    debt_to_liquidation = (
+        liquidation_amount - strat.borrowed()
+    )  ## The debt that will liquidate us
 
-  debt_per_year = strat.borrowed() * currentVariableBorrowRate / (10 ** 27) ## Not compounding so technically underestimate
-  years_to_liquidation = debt_to_liquidation / debt_per_year ## The years to liquidation
-  days_to_liquidation = years_to_liquidation * 365
+    print("debt_to_liquidation")
+    print(debt_to_liquidation)
 
-  print("days to liquidation")
-  print(days_to_liquidation)
+    debt_per_year = (
+        strat.borrowed() * currentVariableBorrowRate / (10 ** 27)
+    )  ## Not compounding so technically underestimate
+    years_to_liquidation = (
+        debt_to_liquidation / debt_per_year
+    )  ## The years to liquidation
+    days_to_liquidation = years_to_liquidation * 365
 
-  print("Current Rewards")
-  print(strat.valueOfRewards())
+    print("days to liquidation")
+    print(days_to_liquidation)
+
+    print("Current Rewards")
+    print(strat.valueOfRewards())
